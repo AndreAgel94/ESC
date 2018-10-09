5857e6eb9f00e192fe296d6b326d5d4c21d0fc2b
AndreAgel94 <42682732+AndreAgel94@users.noreply.github.com>
2018-10-09 13:09:52 -0300

    Add files via upload

---
+++ 04/fill/Fill.asm
@@ -0,0 +1,50 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
+
+        (RESET)
+	@SCREEN
+	D = A
+	@i
+	M = D
+
+(FOR)
+	@i
+	M = M - 1
+	D = M
+	@SCREEN
+	D = D - M
+	@RESET
+	D;JLE
+	@KBD
+	D = M
+	@BLACK
+	D;JGT
+	@WHITE
+	D;JEQ
+	
+(BLACK)
+	@24576
+	D = A
+	@i
+	A = D - M
+	M = -1
+	@FOR
+	0;JMP
+(WHITE)
+	@24576
+	D = A
+	@i
+	A = D - M
+	M = 0
+	@FOR
+	0;JMP
